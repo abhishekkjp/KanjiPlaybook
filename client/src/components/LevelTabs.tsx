b0b6d726662d746ce293ea9da9d3b4edb0e78e73
KUMAR <kumarabhishekjp222@gmail.com>
2026-05-07 16:04:15 +0900

    NavBar responsive

--- client/src/components/LevelTabs.tsx
+++ client/src/components/LevelTabs.tsx
@@ -9,17 +9,17 @@ const LEVELS: JLPTLevel[] = ['N5', 'N4', 'N3', 'N2', 'N1'];
 
 const LevelTabs = ({ activeLevel, onLevelChange }: Props) => {
   return (
-    <div className="flex  bg-black  justify-center flex-wrap">
+    <div className="flex  px-1 py-2 bg-black  justify-center flex-wrap rounded-sm">
       
       {LEVELS.map((level) => (    
           <button
           key={level}
           onClick={() => onLevelChange(level)}
-          className={`px-6 py-2  flex-1 font-bold text-sm text-white transition-all duration-200
+          className={`px-6 py-2  flex-1 font-bold cursor-pointer text-sm text-white relative transition-all duration-200
             ${
               activeLevel === level
-                ? ''
-                : ''
+                ? 'bg-gray-700 text-white rounded-md shadow-lg [text-shadow:0_0_8px_rgba(255,255,255,0.9)]'
+                : 'text-gray-400 hover:text-white'
             }`}
         >
           {level}
