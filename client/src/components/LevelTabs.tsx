275d17de9fa09afcd7c2a5a3d365957527a801a6
KUMAR <kumarabhishekjp222@gmail.com>
2026-05-08 13:21:59 +0900

    Error resolved

--- client/src/components/LevelTabs.tsx
+++ client/src/components/LevelTabs.tsx
@@ -9,16 +9,16 @@ const LEVELS: JLPTLevel[] = ['N5', 'N4', 'N3', 'N2', 'N1'];
 
 const LevelTabs = ({ activeLevel, onLevelChange }: Props) => {
   return (
-    <div className="flex  px-1 py-2 bg-black  justify-center flex-wrap rounded-sm">
+    <div className="flex  px-2 py-2 bg-black  justify-center flex-wrap rounded-sm">
       
       {LEVELS.map((level) => (    
           <button
           key={level}
           onClick={() => onLevelChange(level)}
-          className={`px-6 py-2  flex-1 font-bold cursor-pointer text-sm text-white relative transition-all duration-200
+          className={`px-6 py-2  flex-1 font-bold cursor-pointer text-sm text-white relative transition-all duration-500
             ${
               activeLevel === level
-                ? 'bg-gray-700 text-white rounded-md shadow-lg [text-shadow:0_0_8px_rgba(255,255,255,0.9)]'
+                ? 'bg-gray-500 text-white rounded-md shadow-lg [text-shadow:0_0_8px_rgba(255,255,255,0.9)]'
                 : 'text-gray-400 hover:text-white'
             }`}
         >
