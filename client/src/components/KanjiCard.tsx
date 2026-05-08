85daf5ca7d3921ed9691346a72919d403535e798
KUMAR <kumarabhishekjp222@gmail.com>
2026-05-08 14:57:39 +0900

    reading updated

--- client/src/components/KanjiCard.tsx
+++ client/src/components/KanjiCard.tsx
@@ -20,19 +20,22 @@ const KanjiCard = ({ kanji }: Props) => {
       </p>
 
       {/* readings */}
-      <div className="text-xs space-y-1 grid grid-cols-2  place-items-center">
-        <div className=' w-full flex flex-col items-center justify-center'>
-          <span className='px-5 py-2  text-white bg-gray-500'>Onyomi</span>
+      <div className="text-xs space-y-1 grid grid-cols-2  place-items-center ">
+    
+    
+        <div className=' w-full flex flex-col items-center justify-center border-r-2 border-dashed border-green-700'>
+          <span className='px-5 py-2  text-white bg-gray-500 mb-4'>Onyomi</span>
           {kanji.onyomi.length > 0 && (
             <div className="flex gap-1 flex-wrap">
-              <span className="text-gray-400 font-medium">音:</span>
+              {/* <span className="text-gray-400 font-medium">音:</span> */}
               <span className="text-gray-600">{kanji.onyomi.join('、')}</span>
             </div>
           )}
         </div>
 
 
-        <div>
+        <div className=' w-full flex flex-col items-center justify-center'>
+        <span className='px-5 py-2  text-white bg-gray-500 mb-4'>Kunyomi</span>
           {kanji.kunyomi.length > 0 && (
             <div className="flex gap-1 flex-wrap">
               <span className="text-gray-400 font-medium">訓:</span>
